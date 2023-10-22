ba1036a0f1a192f1eabcb59a3f1ac9f5b2459020
John <78293728+Tostibrand@users.noreply.github.com>
2023-10-22 09:40:48 +0200

    practice

--- BlankPracticeFiles/script.js
+++ BlankPracticeFiles/script.js
@@ -1,24 +1,5 @@
-let btn = document.querySelector('#btn');
+const button = document.querySelector(`#btn`)
 
-btn.addEventListener('click', (event) => {
-    alert(event.type + `Clicked!`); // click
-});
 
-addEventListener('DOMContentLoaded', (event) => {
-    console.log('The DOM is fully loaded.');
-});
-
-addEventListener('load', (event) => {
-    console.log('The page is fully loaded.');
-});
-
-addEventListener('beforeunload', (event) => {
-    // show the confirmation dialog
-    event.preventDefault();
-    // Google Chrome requires returnValue to be set.
-    event.returnValue = '';
-});
-
-addEventListener('unload', (event) => {
-    // send analytic data
-});
+button.addEventListener('contextmenu', (e) => {
+    e.preventDefault();})
